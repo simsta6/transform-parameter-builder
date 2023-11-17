78c1cf22ccf464379f661ab8d727b87c71da1975
Mindaugas Dirgincius <mindaugas.dirgincius@bentley.com>
2023-11-17 13:24:19 +0200

    Add compresed id64 set to legacy views

--- src/TransformParametersUtils.ts
+++ src/TransformParametersUtils.ts
@@ -25,8 +25,8 @@ export function parseSavedView(savedView: SavedView, viewMode: ViewModes): Trans
 export function parseLegacySavedView(legacyViewDefinition: LegacyView, viewMode: ViewModes): TransformParameters {
   console.log("bbb");
   return {
-    categories: legacyViewDefinition.categorySelectorProps.categories,
-    models: legacyViewDefinition.modelSelectorProps?.models || [],
+    categories: getListFromListOrCompressedId64Set(legacyViewDefinition.categorySelectorProps.categories),
+    models: getListFromListOrCompressedId64Set(legacyViewDefinition.modelSelectorProps?.models),
     neverDrawn: legacyViewDefinition.emphasizeElementsProps?.neverDrawn,
     alwaysDrawn: legacyViewDefinition.emphasizeElementsProps?.alwaysDrawn,
     subCategoryOvr: legacyViewDefinition.displayStyleProps.jsonProperties?.styles?.subCategoryOvr as SubCategoryOverrideData[],
